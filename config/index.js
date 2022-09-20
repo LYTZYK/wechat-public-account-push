3e14dc388647145c4e5ef6b4c7f2e3a7bf62c58f
LYTZYK <113954280+LYTZYK@users.noreply.github.com>
2022-09-20 15:32:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oGhCa6HLCrl54RV-JYJZpOhahHD0",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "hpjKw0EkcJ90cBcrSDOAfXX953Vh4wCcDKo4LyM8L4Q",
       // 所在省份
       province: "四川",
       // 所在城市
