e1adf7f3cffe1e225e7f756bd529cebc04283385
LYTZYK <113954280+LYTZYK@users.noreply.github.com>
2022-09-20 15:44:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "hpjKw0EkcJ90cBcrSDOAfXX953Vh4wCcDKo4LyM8L4Q",
+  APP_ID: "wx587cb2647b6f167c",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "307826b08982ffc08fd34eb030a033f7",
+  APP_SECRET: "f443be51093db72dce78d36a8130f55f",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
